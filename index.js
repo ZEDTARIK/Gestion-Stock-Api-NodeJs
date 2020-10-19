3c5db8728d89d277c821f6c9dbd002b6bf8a0f56
zouhair <ettarak.zouhair@gmail.com>
2020-10-19 09:11:04 +0100

    Add database

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const express = require('express');
 const app = express();
 //const mongoose = require('mongoose');
 const authRegister = require('./routes/auth');
+const product = require('./routes/product');
 
 //MidllWare
 app.use(express.json());
 app.use('/api/user', authRegister);
+app.use('/api/products', product);
 
 const port = process.env.port || 3000;
 app.listen(port, () => console.log(`App working in Port :  http://localhost:/${port}`))
